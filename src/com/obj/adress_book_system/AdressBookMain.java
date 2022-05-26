3e23b695fcf4f6c0e92b5d54f75697d3847fdbce
Anicod <animeshraj735@gmail.com>
2022-05-27 00:23:01 +0530

    added: welcome to the address book

---
+++ src/com/obj/adress_book_system/AdressBookMain.java
@@ -0,0 +1,8 @@
+package com.obj.adress_book_system;
+
+public class AdressBookMain {
+    public static void main(String[] args) {
+        System.out.println("welcome to the Address book program");
+
+    }
+}
