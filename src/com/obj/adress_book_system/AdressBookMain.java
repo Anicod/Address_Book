b900f89b3177f18ac6721597add186d518cd34b6
Anicod <animeshraj735@gmail.com>
2022-06-14 11:02:51 +0530

    added: details are display

--- src/com/obj/adress_book_system/AdressBookMain.java
+++ src/com/obj/adress_book_system/AdressBookMain.java
@@ -2,6 +2,34 @@ package com.obj.adress_book_system;
 
 public class AdressBookMain {
     public static void main(String[] args) {
-        System.out.println("welcome to the address book");
+
+        ProcessAddressBook processAddressBook = new ProcessAddressBook();
+
+        while (true) {
+
+            processAddressBook.takingInputOverWhileLoop();
+
+            switch (processAddressBook.choiceOfUsers) {
+
+                case ProcessAddressBook.ADD_DETAILS:
+                    processAddressBook.addDetails();
+                    break;
+                case ProcessAddressBook.PRINT_DETAILS:
+                    processAddressBook.displayPersonDetails();
+                    break;
+                case ProcessAddressBook.EDIT_DETAILS:
+                    processAddressBook.editDetails();
+                    break;
+                case ProcessAddressBook.DELETE_DETAILS:
+                    processAddressBook.deleteDetails();
+                    break;
+                case ProcessAddressBook.EXIT_PROGRAM:
+                    processAddressBook.displayTermination();
+                    return;
+                default:
+                    System.out.println("Please Enter correct input !");
+                    break;
+            }
+        }
     }
 }
